504bfdf2ebe436c7a68d56dad891e5f20e5c3d24
DrakeO <drake.olejniczak@gmail.com>
2024-11-12 09:12:08 -0500

    Add new csv data and define new dir structure

---
+++ merge_pop_wetland_road.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("Starting script to calculate wetland area by county.")
+
+
